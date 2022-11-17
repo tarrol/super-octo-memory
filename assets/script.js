b5a5f97248a0cd47b04a3f3ea1e0b4e9f19a7af5
Andrey Skorodumov <quickandrey@gmail.com>
2022-11-16 23:29:56 -0800

    Created functional logic for the game to switch between questions

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,11 @@
 var startGame = $('#startGame');
+
 var timeRemaining = 60
+var rightAnswers = 0
+var wrongAnswers = 0
+var questionAnswered = false
+var intervalExist = false
+
 var option1 = $('#opt1')
 var option2 = $('#opt2')
 var option3 = $('#opt3')
@@ -19,13 +25,25 @@ function question1() {
   option4.text('Blueberry')
 }
 function question2() {
-
+  question.text('Which of these is a fruit?')
+  option1.text('Rock')
+  option2.text('Banana')
+  option3.text('Stick')
+  option4.text('Blueberry')
 }
 function question3() {
-
+  question.text('Which of these is a fruit?')
+  option1.text('Banana')
+  option2.text('Rock')
+  option3.text('Stick')
+  option4.text('Blueberry')
 }
 function question3() {
-
+  question.text('Which of these is a fruit?')
+  option1.text('Banana')
+  option2.text('Rock')
+  option3.text('Stick')
+  option4.text('Blueberry')
 }
 
 
@@ -36,20 +54,35 @@ function startTimer() {
     $('#time').text(timeRemaining)
     if (timeRemaining <= 0) {
       clearInterval(theInterval)
+      intervalExist = false
       $('#gameState').text("Sorry! You lost.")
+
     }
   }
   var theInterval = setInterval(() => {
     myTimer();
   }, 1000);
+  intervalExist = true
+}
+
+
+function nextQuestion() {
+  if (wrongAnswers + rightAnswers === 0) {
+    if (questionAnswered == true && response1 == true) { rightAnswers++; answerReset; question2(); } else if (response1 != true && questionAnswered == true) { wrongAnswers++; answerReset; question2(); }
+  }
+
 }
 
 function startQuiz() {
+  answerReset()
+  wrongAnswers = 0
+  rightAnswers = 0
   startTimer()
   question1()
   $('#optList').removeClass('d-none')
-
 }
+
+
 
 function answerReset() {
   response1 = false
@@ -59,19 +92,31 @@ function answerReset() {
 }
 
 function alert1() {
+  console.log("btn1 pressed");
   response1 = true
+  questionAnswered = true
+  nextQuestion();
 }
 
 function alert2() {
+  console.log("btn2 pressed");
+  questionAnswered = true
   response2 = true
+  nextQuestion();
 }
 
 function alert3() {
+  console.log("btn3 pressed");
+  questionAnswered = true
   response3 = true
+  nextQuestion();
 }
 
 function alert4() {
+  console.log("btn4 pressed");
+  questionAnswered = true
   response4 = true
+  nextQuestion();
 }
 
 startGame.on("click", startQuiz);
